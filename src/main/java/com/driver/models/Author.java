9046ca7404adf74e7c1e508387744bd60e1c8c17
Nikitha <nikithamashetti434@gmail.com>
2022-12-04 10:37:02 +0530

    push

--- src/main/java/com/driver/models/Author.java
+++ src/main/java/com/driver/models/Author.java
@@ -6,9 +6,6 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.util.List;
-@Getter
-@Setter
-@Builder
 @ToString
 @AllArgsConstructor
 @Entity
@@ -36,6 +33,46 @@ public Author(String name,String email,int age,String country){
         this.email=email;
         this.age=age;
         this.country=country;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
 
